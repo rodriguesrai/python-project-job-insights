a573c4cdf31e71476fb9f851c133cad6992f85c6
rodriguesrai <rairodrigues_sf@hotmail.com>
2024-02-27 08:55:13 -0300

    fix test file

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,17 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> List[Dict[str, str]]:
+        with open(path, "r") as file:
+            csv_reader = csv.DictReader(file)
+            self.jobs_list = list(csv_reader)
+
+        return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
--- tests/test_counter.py
+++
@@ -1,6 +0,0 @@
-from src.pre_built.counter import count_ocurrences
-
-
-def test_count_ocurrences():
-    pathfile = "data/jobs.csv"
-    assert count_ocurrences(pathfile, "test") == 2687
